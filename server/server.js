332f5515c5703d4c14d4020dc55be20449dc1663
elferno <elferno@inbox.ru>
2021-08-22 13:38:37 +0500

    base reload + watch + node + express

---
+++ server/server.js
@@ -0,0 +1,32 @@
+// import
+import express from 'express'
+import {createServer} from 'http'
+import {join, resolve} from 'path'
+import reload from 'reload'
+import watch from 'watch'
+
+// var
+const port = 3000
+const started_log = () => console.log(`server rised on port ${port} ...`)
+
+const app = express()
+const baseDir = join(resolve(), 'client')
+const homePage = join(baseDir, 'index.html')
+
+// server prefs
+app.use(express.json())
+app.use(express.static(baseDir))
+
+// home page
+app.get('/', (req, res) => res.sendFile(homePage))
+
+// create server
+const startServer = async () => {
+  const server = createServer(app)
+  const reloadReturned = await reload(app)
+  server.listen(port, started_log)
+  watch.watchTree(baseDir, () => reloadReturned.reload())
+}
+
+// start
+startServer()
